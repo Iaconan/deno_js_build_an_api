5e0b8f69c9d5e16b87edccaa48849e9ddb7eccab
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-04-29 19:25:33 +0200

    index.js is done now we should we run our deno.js

--- router.js
+++ router.js
@@ -1,1 +1,10 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+
+const router = new Router();
+
+router.get('/', ({ response }) => {
+    response.body = 'Todo List API Using Deno Runtime.';
+});
+
+
+export default router;
